e9fa7122b346a283cc7725c607198d9176da7dee
gabriel <gabriel.azevedo@soulasalle.com.br>
2019-05-27 21:07:54 -0300

    mensagem

---
+++ enviagit.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/bash/env bash
+git pull
+git add *
+git commit -m "mensagem"
+git push
---
+++ soma2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "num1:12"
+
+num1=12
+
+echo "num2:10"
+num2=10
+
+soma=$(($num1+$num2))
+echo " valor da soma sera $soma"
+
